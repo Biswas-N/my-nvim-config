033f82208470ad3a76dab904030ba82a1e50562a
Biswas Nandamuri <biswas.bin2@gmail.com>
2023-08-24 23:04:45 -0500

    add autocomplete and lsp config

---
+++ lua/plugins/lsp.lua
@@ -0,0 +1,23 @@
+return {
+  -- add symbols-outline
+  {
+    "simrat39/symbols-outline.nvim",
+    cmd = "SymbolsOutline",
+    keys = { { "<leader>cs", "<cmd>SymbolsOutline<cr>", desc = "Symbols Outline" } },
+    opts = {
+      -- add your options that should be passed to the setup() function here
+      position = "right",
+    },
+  },
+  {
+    "neovim/nvim-lspconfig",
+    ---@class PluginLspOpts
+    opts = {
+      ---@type lspconfig.options
+      servers = {
+        -- pyright will be automatically installed with mason and loaded with lspconfig
+        pyright = {},
+      },
+    },
+  },
+}
