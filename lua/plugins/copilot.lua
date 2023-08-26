b954e5dab1f498196537f93c51ec71c8f952a792
Biswas Nandamuri <biswas.bin2@gmail.com>
2023-08-26 12:58:23 -0500

    fix copilot keymap

--- lua/plugins/copilot.lua
+++ lua/plugins/copilot.lua
@@ -2,7 +2,12 @@ return {
   {
     "github/copilot.vim",
     config = function()
-      vim.keymap.set("i", "<C-j>", "copilot#Accept()", { expr = true })
+      vim.keymap.set(
+        "i",
+        "<C-j>",
+        "copilot#Accept('<CR>')",
+        { noremap = true, silent = true, expr = true, replace_keycodes = false }
+      )
     end,
   },
 }
