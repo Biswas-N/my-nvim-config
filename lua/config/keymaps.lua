41bbb56962791293d241ecdcf11106aaf9ab1b2c
Biswas Nandamuri <biswas.bin2@gmail.com>
2023-11-28 17:50:53 -0800

    remove custom save and quit keymaps

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -7,5 +7,3 @@ vim.keymap.del("n", "<leader>L") -- Delete a keymap set by LazyVim defaults
 vim.keymap.set("n", "<leader>L", "<cmd>Lazy<cr>", { noremap = true, silent = true })
 vim.keymap.set({ "n", "v" }, "<leader>l", "$h", { noremap = true, silent = true })
 vim.keymap.set({ "n", "v" }, "<leader>h", "0", { noremap = true, silent = true })
-vim.keymap.set("n", "<leader>w", "<cmd>w<cr>", { noremap = true, silent = true })
-vim.keymap.set("n", "<leader>q", "<cmd>q<cr>", { noremap = true, silent = true })
