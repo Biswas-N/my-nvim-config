3714b72b394de9bf66d03307e904708dcfdf0f5b
Biswas Nandamuri <bnandamuri@trivie.com>
2024-07-24 16:20:34 +0000

    remove osc52 support for Mac

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -2,17 +2,3 @@
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
 vim.opt.clipboard = "unnamedplus"
-
-local function copy(lines, _)
-  require("osc52").copy(table.concat(lines, "\n"))
-end
-
-local function paste()
-  return { vim.fn.split(vim.fn.getreg(""), "\n"), vim.fn.getregtype("") }
-end
-
-vim.g.clipboard = {
-  name = "osc52",
-  copy = { ["+"] = copy, ["*"] = copy },
-  paste = { ["+"] = paste, ["*"] = paste },
-}
