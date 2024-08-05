bdd7677151c1b84c49bc901ac6632c22335de198
Biswas Nandamuri <biswas.bin2@gmail.com>
2024-08-05 12:24:03 -0500

    add copilot to autocompletions

--- lua/plugins/completions.lua
+++ lua/plugins/completions.lua
@@ -1,5 +1,18 @@
 return {
   {
+    "zbirenbaum/copilot.lua",
+    cmd = "Copilot",
+    build = ":Copilot auth",
+    event = "InsertEnter",
+    opts = {
+      suggestion = { enabled = false },
+      panel = { enabled = false },
+      filetypes = {
+        ["*"] = true,
+      },
+    },
+  },
+  {
     "hrsh7th/nvim-cmp",
     dependencies = {
       "saadparwaiz1/cmp_luasnip",
@@ -9,33 +22,90 @@ return {
       "hrsh7th/cmp-buffer",
       "hrsh7th/cmp-path",
       "hrsh7th/cmp-cmdline",
+      "zbirenbaum/copilot.lua",
+      "zbirenbaum/copilot-cmp",
+      "onsails/lspkind-nvim",
     },
     config = function()
       -- Set up nvim-cmp.
-      local cmp = require("cmp")
+      local cmp, luasnip, lspkind = require("cmp"), require("luasnip"), require("lspkind")
+
+      -- Enable dependencies
       require("luasnip.loaders.from_vscode").lazy_load() -- loading friendly snippets
+      require("copilot_cmp").setup()
+
+      local has_words_before = function()
+        unpack = unpack or table.unpack
+        local line, col = unpack(vim.api.nvim_win_get_cursor(0))
+        if col == 0 then
+          return false
+        end
+        local current_line = vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]
+        return not current_line:sub(col, col):match("%s")
+      end
 
       cmp.setup({
         snippet = {
           -- REQUIRED - you must specify a snippet engine
           expand = function(args)
-            require("luasnip").lsp_expand(args.body) -- For `luasnip` users.
+            luasnip.lsp_expand(args.body) -- For `luasnip` users.
           end,
         },
         window = {
-          -- completion = cmp.config.window.bordered(),
+          -- completion = cmp.config.window.unbordered(),
           -- documentation = cmp.config.window.bordered(),
+        },
+        formatting = {
+          format = lspkind.cmp_format({
+            mode = "symbol_text", -- Show symbol and text
+            maxwidth = 50,   -- Prevent the popup from showing more than provided characters (e.g 50 will not show more than 50 characters)
+            ellipsis_char = "...", -- When popup menu exceed maxwidth, the truncated part will show ellipsis_char instead
+            symbol_map = { Copilot = "" },
+          }),
         },
         mapping = cmp.mapping.preset.insert({
           ["<C-b>"] = cmp.mapping.scroll_docs(-4),
           ["<C-f>"] = cmp.mapping.scroll_docs(4),
           ["<C-Space>"] = cmp.mapping.complete(),
           ["<C-e>"] = cmp.mapping.abort(),
-          ["<CR>"] = cmp.mapping.confirm({ select = true }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
+          ["<CR>"] = cmp.mapping(function(fallback)
+            if cmp.visible() then
+              if luasnip.expandable() then
+                luasnip.expand()
+              else
+                cmp.confirm({
+                  select = true,
+                })
+              end
+            else
+              fallback()
+            end
+          end),
+          ["<Tab>"] = cmp.mapping(function(fallback)
+            if cmp.visible() then
+              cmp.select_next_item()
+            elseif luasnip.expand_or_jumpable() then
+              luasnip.expand_or_jump()
+            elseif has_words_before() then
+              cmp.complete()
+            else
+              fallback()
+            end
+          end, { "i", "s" }),
+          ["<S-Tab>"] = cmp.mapping(function(fallback)
+            if cmp.visible() then
+              cmp.select_prev_item()
+            elseif luasnip.jumpable(-1) then
+              luasnip.jump(-1)
+            else
+              fallback()
+            end
+          end, { "i", "s" }),
         }),
         sources = cmp.config.sources({
           { name = "nvim_lsp" },
           { name = "luasnip" }, -- For luasnip users.
+          { name = "copilot" }, -- For Github Copilot.
         }, {
           { name = "buffer" },
         }),
