1ca9ba17e6d79a79671580ae8c8773908502bdeb
Biswas Nandamuri <biswas.bin2@gmail.com>
2024-09-19 14:24:37 -0500

    add indent-blankline and remove mini.indent

--- lua/plugins/ui.lua
+++ lua/plugins/ui.lua
@@ -136,4 +136,17 @@ return {
       -- refer to the configuration section below
     },
   },
+  {
+    "lukas-reineke/indent-blankline.nvim",
+    main = "ibl",
+    ---@module "ibl"
+    ---@type ibl.config
+    opts = {
+      indent = { char = "▏" },
+      scope = {
+        show_start = false,
+        show_end = false,
+      },
+    },
+  },
 }
--- lua/plugins/utils.lua
+++ lua/plugins/utils.lua
@@ -71,31 +71,6 @@ return {
       -- Enable sub-plugins
       require("mini.surround").setup()
       require("mini.comment").setup()
-      require("mini.indentscope").setup({
-        symbol = "│",
-        options = { try_as_border = true },
-      })
-    end,
-    init = function()
-      vim.api.nvim_create_autocmd("FileType", {
-        pattern = {
-          "alpha",
-          "dashboard",
-          "fzf",
-          "help",
-          "lazy",
-          "lazyterm",
-          "mason",
-          "neo-tree",
-          "notify",
-          "toggleterm",
-          "Trouble",
-          "trouble",
-        },
-        callback = function()
-          vim.b.miniindentscope_disable = true
-        end,
-      })
     end,
   },
   {
